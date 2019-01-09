7598a09a0f467248e2ce0c0872534af826ce6956
Jabari Holder <jholder@and.digital>
2019-01-09 15:04:11 +0000

    Update block to receive img and text

--- card-block.php
+++ card-block.php
@@ -1,15 +1,20 @@
 <?php
-   /*
-   Plugin Name: Card Block
-   */
+/**
+ * Plugin Name: Gutenvko Block
+ * Plugin URI: https://github.com/anddigital-jh/Gutenvko
+ * Description: Mr.Zhivko's Gutenburg Block
+ * Author: Jabari Holder
+ * Author URI: http://www.jabari-holder.com
+ * Version: 1.0.0
+ */
 
-  function my_register_gutenberg_card_block() {
+function my_register_gutenberg_card_block() {
 
    // Register our block script with WordPress
-   wp_register_script(
+   wp_enqueue_script(
      'gutenberg-card-block',
      plugins_url('/blocks/dist/blocks.build.js', __FILE__),
-     array('wp-blocks', 'wp-element')
+     array('wp-blocks', 'wp-element', 'wp-editor')
    );
  
    // Register our block's base CSS  
@@ -25,13 +30,18 @@
      plugins_url('/blocks/dist/blocks.editor.build.css', __FILE__),
      array( 'wp-edit-blocks' )
    );  
-   
-   // Enqueue the script in the editor
-   register_block_type('card-block/main', array(
-     'editor_script' => 'gutenberg-card-block',
-     'editor_style' => 'gutenberg-card-block-edit-style',
-     'style' => 'gutenberg-card-block-edit-style'
-   ));
  }
  
- add_action('init', 'my_register_gutenberg_card_block');
+function register_card_block() {
+	if ( function_exists( 'register_block_type' ) ) {
+    // Enqueue the script in the editor
+    register_block_type('card-block/main', array(
+      'editor_script' => 'gutenberg-card-block',
+      'editor_style' => 'gutenberg-card-block-edit-style',
+      'style' => 'gutenberg-card-block-edit-style'
+    ));
+	}
+}
+
+add_action('enqueue_block_assets', 'my_register_gutenberg_card_block');
+add_action( 'plugins_loaded', 'register_card_block');
